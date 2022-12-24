e31c4939b7aa2db583320c6d8f91e1cd053f97b6
Yousuf9963 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-24 08:39:56 +0300

    Update ip-shower.py

--- ip-shower.py
+++ ip-shower.py
@@ -1,4 +1,4 @@
-print("[+] Tool Namee:IP-SHOWER")
+print("[+] Tool Name:IP-SHOWER")
 
 print("[+] Author:Yousuf Shafi'i Muhammad. Junior Programmer")
 
@@ -10,11 +10,11 @@ print("[+] Github:https://github.com/Yousuf9963/phone-num-info")
 
 print("[+] Follow me on Github: https://github.com/Yousuf9963")
 
-print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
+print("[-] Website: muhammadabdirahman.wixsite.com/yousuf9963blog.")
 
 print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
 
-print("[+] I hope for you good future and i am willing that you will come high effort.")
+print("Motto: [+] I hope for you good future and i am willing that you will come high effort.")
 
 print("")
 
